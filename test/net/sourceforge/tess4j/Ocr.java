9bcda2258a1e0faf3d3a90c7fc87bf1b65629dd7
dom <dengyong_131@qq.com>
2017-10-25 19:31:39 +0800

    init

--- test/net/sourceforge/tess4j/Ocr.java
+++ test/net/sourceforge/tess4j/Ocr.java
@@ -4,13 +4,17 @@ import java.io.File;
 
 public class Ocr {
 	public static void main(String[] args) {
-		File root = new File("C:\\Users\\Dom\\Pictures\\cmbc");
+		File root = new File("C:\\Users\\Dom\\Pictures\\icbc");
 		ITesseract instance = new Tesseract();
-		char[] specialCharacters = { '_', '\'', ',', '"', '“', ' ', '’', '-', '*', '\\', ']', '}' };
+		String[] specialCharacters = { ":", "(", ")", "?", "_", "'", "‘", ",", "\"", "“", " ", "’", "-", "*", "\\", "[",
+				"]", "{", "}", "\n", "é", ".", "!", "%", ";", "|", "<", ">" };
 		try {
 			File[] files = root.listFiles();
 			for (File file : files) {
 				String result = instance.doOCR(file);
+				for (String s : specialCharacters) {
+					result = result.replace(s, "");
+				}
 				String fileName = file.toString().substring(file.toString().lastIndexOf("\\") + 1);
 				System.out.println("图片名：" + fileName + " 识别结果：" + result);
 			}
